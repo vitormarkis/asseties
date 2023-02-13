c8ce4aba9965a34e5c966f1282c43715e73508ed
Vitor Markis <vitormarkis2369@gmail.com>
2023-02-13 10:33:43 -0300

    add styles to svg in toolbar

--- src/routes/assetList.tsx
+++ src/routes/assetList.tsx
@@ -1,10 +1,11 @@
+import { Sort } from "@assets/icons"
 import { Asset } from "@components/Asset"
 import { baseURL } from "@constants/constants"
 import { AssetType } from "@features/asset-slice/types"
 import axios from "axios"
+import clsx from "clsx"
 import { useState } from "react"
 import { useQuery } from "react-query"
-import { Sort } from '@assets/icons'
 
 interface Props {
   toolbar?: boolean
@@ -12,6 +13,7 @@ interface Props {
 
 export const AssetList: React.FC<Props> = ({ toolbar }) => {
   const [container, setContainer] = useState<null | Element>(null)
+  const [ordering, setOrdering] = useState(false);
 
   const {
     data: assets,
@@ -39,22 +41,39 @@ export const AssetList: React.FC<Props> = ({ toolbar }) => {
   return (
     <div>
       {toolbar && (
-        <div className="h-16 bg-blue-600">
-          <Sort width={20} />
+        <div className="h-16 w-[560px] text-sm p-4 rounded-lg mb-4 bg-zinc-200 flex justify-between">
+          <div></div>
+          <div className="flex gap-2">
+            <div 
+            onClick={() => setOrdering(old => !old)}
+            className={
+              clsx(
+                "p-1 hover:bg-black/10 flex items-center h-8 w-8 justify-center rounded-full", 
+                {
+                  'bg-black/10': ordering,
+                }
+                )}>
+              <Sort
+                className="cursor-pointer"
+                width={20}
+                color={"#020202"}
+              />
+            </div>
+          </div>
         </div>
       )}
       <div
-      ref={setContainer}
-      className="w-[560px] text-sm p-4 rounded-lg bg-zinc-200 h-fit overflow-y-scroll"
-    >
-      {assets?.map(asset => (
-        <Asset
-          key={asset.id}
-          container={container}
-          asset={asset}
-        />
-      ))}
-    </div>
+        ref={setContainer}
+        className="w-[560px] text-sm p-4 rounded-lg bg-zinc-200 h-fit overflow-y-scroll"
+      >
+        {assets?.map(asset => (
+          <Asset
+            key={asset.id}
+            container={container}
+            asset={asset}
+          />
+        ))}
+      </div>
     </div>
   )
 }
