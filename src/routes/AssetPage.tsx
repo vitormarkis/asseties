9b56889df3f7ecb3c1f0970437327dd4e4b16e60
Vitor Markis <vitormarkis2369@gmail.com>
2023-02-23 23:23:08 -0300

    add back button to assetpage

--- src/routes/AssetPage.tsx
+++ src/routes/AssetPage.tsx
@@ -1,4 +1,5 @@
 import AssetPageDetails from "@components/AssetPageDetails"
+import { Button } from "@components/atoms"
 import ListAsset from "@components/ListAssets"
 import Navbar from "@components/Navbar"
 import { baseURL } from "@constants/constants"
@@ -6,15 +7,16 @@ import { setLastAssetCacheId } from "@features/context-slice"
 import { useAppSelector } from "@features/store"
 import axios from "axios"
 import { useDispatch } from "react-redux"
-import { LoaderFunctionArgs, useParams } from "react-router-dom"
+import { LoaderFunctionArgs, useNavigate, useParams } from "react-router-dom"
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { id } = params ?? {}
-  return id ? await axios.get(baseURL + '/' + id) : null
+  return id ? await axios.get(baseURL + "/" + id) : null
 }
 
 const AssetPage: React.FC = () => {
   const { id } = useParams() as { id: string }
+  const navigate = useNavigate()
   const { last_asset_cache_id } = useAppSelector(state => state.context)
   const dispatch = useDispatch()
   dispatch(setLastAssetCacheId(last_asset_cache_id ?? id))
@@ -29,6 +31,13 @@ const AssetPage: React.FC = () => {
         style={{ gridArea: "main" }}
         className="p-4"
       >
+        <Button
+          bg="green"
+          rounded="full"
+          textColor="white"
+          value="Voltar"
+          onClick={() => navigate(-1)}
+        />
         {id ? <AssetPageDetails id={id} /> : <ListAsset />}
       </div>
     </div>
